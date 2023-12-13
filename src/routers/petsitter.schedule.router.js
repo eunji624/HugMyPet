baccebc994778f40e1de7c79916aae3a1630312e
Junsik <chzhgod@gmail.com>
2023-12-13 13:55:43 +0900

    :docs: [petsitter] routers 디렉토리로 파일 이동

---
+++ src/routers/petsitter.schedule.router.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const scheduleRouter = express.Router();
+
+import { PetsitterScheduleController } from '../controller/petsitter.schedule.controller.js';
+const petSitterScheduleController = new PetsitterScheduleController();
+
+/* 스케쥴 추가하기 */
+scheduleRouter.post('/', petSitterScheduleController.setSchedules)
+
+
+/* 특정 펫시터 예약 스케쥴 가져오기 */
+scheduleRouter.get('/:petSitterId', petSitterScheduleController.getSchedules);
+
+export default scheduleRouter;
