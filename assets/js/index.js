0048cb0f3e3e6f1a01196ab980e19296550cb516
Junsik <chzhgod@gmail.com>
2023-12-15 00:06:17 +0900

    feat: [front/main2] main 페이지 2차 작업 완료
    1) 서버에 요청하여 펫시터들 정보 가져오기
    2) 카드 형태로 뿌리기
    - 펫시터 소개 데이터 추가 필요
    - 프론트에서 케어 가능한 동물 및 동네 추가 필요
    3) 펫시터 가입하기 버튼 클릭 시 펫시터 가입 페이지 이동

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,64 @@
-/* 펫시터 정보들 조회하기 */
+/* 펫시터 정보들 가져오는 함수 */
+const getTotalPetSitters = async () => {
+  try {
+    const result = await fetch('/api/pet-sitters', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .catch((err) => err);
 
+    return result.data;
+  } catch (err) {
+    console.error(err)
+  }
+};
+
+/* pet-sitter 정의하기 */
+const petSitters = await getTotalPetSitters();
+
+/* 가져온 펫시터 정보를 카드 형태로 뿌려주는 함수 */
+const spreadPetSittersCard = async (petSitters) => {
+  const cardListDiv = $('#petsitter-cards-list');
+  cardListDiv.empty(); // 카드를 뿌리기 전 초기화
+  console.log('petSitters: ', petSitters);
+  petSitters.forEach((petSitter) => {
+
+    const {
+      petSitterId,
+      name,
+      availablePet,
+      availableAddress,
+      score,
+      imagePath
+    } = petSitter;
+
+    const thumbnailPath = imagePath // 나중에 순서 바꾸기
+      ? '../../assets/Img/6.png' : `https://nbcamp-bukkit.s3.ap-northeast-2.amazonaws.com/${images_path.split(',')[0]}`
+
+    const petsitterScore = score === null ? 0 : score;
+
+    cardListDiv.append(
+      `<li class="community-card">
+        <div class="card-thumbnail">
+          <a href="/pet-sitter/${petSitterId}">
+            <img
+              src="${thumbnailPath}"
+            />
+          </a>
+        </div>
+        <div class="card-description">
+          <div class="card-title">
+            <a href="/pet-sitter/${petSitterId}"> ${name} </a>
+          </div>
+          <div class="card-overview">펫시터 소개 부분 추가해주세요 은지님 !!!! </div>
+          <div class="card-footer">
+            <div class="card-score">${petsitterScore}</div>
+            <div class="card-info"></div>
+          </div>
+        </div>
+      </li>`
+    );
+  });
+};
+
+spreadPetSittersCard(petSitters)
