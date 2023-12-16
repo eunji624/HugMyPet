ddeefa48fd6074b836f8293d10e1f811393bfd72
minjae Kim <cmg981548@gmail.com>
2023-12-16 19:17:14 +0900

    fix: js파일 sign-out / sign-in 합체

---
+++ assets/js/sign-out.js
@@ -0,0 +1,98 @@
+async function petsittersignOut() {
+	try {
+		const password = prompt('비밀번호를 입력하세요');
+		if (!password) {
+			alert('비밀번호를 입력하세요.');
+			return;
+		}
+
+		if (password === null) {
+			alert('비밀번호 입력이 취소되었습니다.');
+			return;
+		}
+
+		if (password.trim() === '') {
+			alert('비밀번호를 입력하세요.');
+			return;
+		}
+
+		const accessToken = localStorage.getItem('accessToken');
+		const response = await fetch('api/sign-out/pet-sitters', {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ password })
+		});
+		const data = await response.json();
+
+		if (data.success) {
+			alert('회원탈퇴 완료');
+			localStorage.removeItem('accessToken');
+			window.location.href = '/';
+			return;
+		} else {
+			alert('비밀번호가 다릅니다.');
+			return;
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function membersignOut() {
+	try {
+		const password = prompt('비밀번호를 입력하세요');
+
+		if (!password) {
+			alert('비밀번호를 입력하세요.');
+			return;
+		}
+
+		if (password === null) {
+			alert('비밀번호 입력이 취소되었습니다.');
+			return;
+		}
+
+		if (password.trim() === '') {
+			alert('비밀번호를 입력하세요.');
+			return;
+		}
+
+		const accessToken = localStorage.getItem('accessToken');
+		const response = await fetch('api/sign-out/users', {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ password })
+		});
+
+		const data = await response.json();
+
+		if (data.success) {
+			alert('회원탈퇴 완료');
+			localStorage.removeItem('accessToken');
+			window.location.href = '/';
+		} else {
+			alert('비밀번호가 다릅니다.');
+			return;
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const memberSignOutbtn = document.querySelector('.member-sign-out-btn');
+memberSignOutbtn.addEventListener('click', () => {
+	membersignOut();
+});
+
+const petsitterSignOutbtn = document.querySelector('.petsitter-sign-out-btn');
+petsitterSignOutbtn.addEventListener('click', () => {
+	petsittersignOut();
+});
